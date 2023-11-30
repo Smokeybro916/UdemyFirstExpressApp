8ac619424464dc549490bb30d83e69f2dd1870ac
Smokeybro916 <kyleluangasa@gmail.com>
2023-11-30 16:52:45 -0500

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require("express");
-const app = express()
-console.dir(app)
+const app = express();
 
+app.use((req, res) => {
+    console.log("We got a new request!")
+})
 
+app.get('/r/:subreddit', (req, res) => {
+    console.log(req.params);
+    res.send("This is a subreddit!")
+})
+
+app.get('/', (req, res) => {
+    res.send("This is the home page!")
+})
+
+app.post('/cats', (req, res) => {
+    res.send("Post request to /cats! This is different than a get request!!")
+})
+
+app.get('/cats', (req, res) => {
+    res.send("Meoww!")
+})
+
+app.get('/dogs', (req, res) => {
+    res.send("Woof!")
+})
+
+app.get('*', (req, res) => {
+    res.send(`I don't know that path `)
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000")
